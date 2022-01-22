84e31c033c4803fae6f043d94c0a747103416f0e
hiranya-bhutani <84444999+hiranya-bhutani@users.noreply.github.com>
2022-01-22 22:13:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+canvas=document.getElementById("myCanvas");
+ctx= canvas.getContext("2d");
+
+
+
+ctx.beginPath();
+ctx.strokeStyle = "grey";
+ctx.lineWidth=1;
+ctx.rect(150, 143, 430, 200)
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "blue";
+ctx.lineWidth=5;
+ctx.arc(245, 230, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.lineWidth=5;
+ctx.arc(355, 230, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "red";
+ctx.lineWidth=5;
+ctx.arc(465, 230, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "gold";
+ctx.lineWidth=5;
+ctx.arc(300, 260, 40, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = "green";
+ctx.lineWidth=5;
+ctx.arc(410, 260, 40, 0, 2*Math.PI);
+ctx.stroke();
